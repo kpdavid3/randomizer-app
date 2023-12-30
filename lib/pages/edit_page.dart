db96c9e944b1d0be7b8d2e885a84ebe534f01308
kpdavid3 <kpdavid3@up.edu.ph>
2023-12-30 23:22:38 +0800

    feat: Created file picker in Edit Page

--- lib/pages/edit_page.dart
+++ lib/pages/edit_page.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
-class EditPage extends StatelessWidget {
+class EditPage extends StatefulWidget {
   const EditPage({super.key});
+
+  @override
+  _EditPageState createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
+  String? selectedFilePath;
+
+  Future<void> selectFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['xlsx'],
+    );
+
+    if (result != null) {
+      setState(() {
+        selectedFilePath = result.files.single.path;
+      });
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Edit Page'),
       ),
-      body: const Center(
-        child: Text('This is the Edit Page!'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: selectFile,
+              child: const Text('Select Excel File'),
+            ),
+            if (selectedFilePath != null) Text('Selected file: $selectedFilePath'),
+
+          ],
+        ),
       ),
     );
   }
